038690e055c78c4337e5b461aaf6615b906a6fa6
Oleg <o.verkhoturov@gmail.com>
2025-12-17 13:26:38 +0300

    Correct hover

---
+++ app/_components/PortfolioBlock/PortfolioCard.styles.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledPortfolioCard = styled.div`
+  border: 1px solid #DFDEDE;
+  
+  // .portfolio-img {
+  //   background-image: url(/portfolio/pixual.png);
+  //   background-size: contain;
+  //   background-repeat: no-repeat;
+  //   background-position: center;
+  // }
+
+  &:hover {
+    border: 1px solid var(--foreground);
+  }
+
+
+`
--- app/_components/PortfolioBlock/PortfolioCard.tsx
+++ app/_components/PortfolioBlock/PortfolioCard.tsx
@@ -1,6 +1,8 @@
 import Tag from "@/components/Tag";
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useState } from "react";
+import { StyledPortfolioCard } from "./PortfolioCard.styles";
+import classNames from "classnames";
 
 type Props = {
   title: string;
@@ -9,10 +11,27 @@ type Props = {
   additionalTags: string[];
 }
 
-const   PortfolioCard:FC<Props> = (props) => {
+const PortfolioCard:FC<Props> = (props) => {
+  const [isOver, setOver] = useState(false);
   return (
-    <div className="flex flex-col max-w-[824px] w-full 2xl:flex-row items-center gap-4 lg:gap-8 p-8 bg-[#F7F7F7] border border-solid border-[#DFDEDE] rounded-4xl">
-      <Image className="w-full max-w-[364px] max-h-64 transition duration-300 ease-in-out hover:scale-110" src={props.img} alt="project-cover" width={364} height={256} />
+    <StyledPortfolioCard
+      className="flex flex-col max-w-[824px] w-full transition duration-300 ease-in-out 2xl:flex-row items-center gap-4 lg:gap-8 p-8 bg-[#F7F7F7] rounded-4xl"
+      onMouseOver={() => setOver(true)}
+      onMouseOut={() => setOver(false)}
+    >
+      <div className="relative w-full max-w-[364px] max-h-64 overflow-hidden rounded-xl">
+        <Image
+          className={classNames({
+            'porfolio-img w-full h-full object-cover transition duration-300 ease-in-out': true,
+            'scale-110': isOver,
+            'scale-100': !isOver,
+          })}
+          src={props.img}
+          alt="project-cover"
+          width={364}
+          height={256}
+        />
+      </div>
       <div className="max-w-[364px] w-full">
         <h3 className="font-medium text-[32px] leading-10 mb-3">{props.title}</h3>
 
@@ -28,7 +47,7 @@ const   PortfolioCard:FC<Props> = (props) => {
           ))}
         </div>
       </div>
-    </div>
+    </StyledPortfolioCard>
   )
 }
 
